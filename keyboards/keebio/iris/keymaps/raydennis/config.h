0e80e1ddb05cb238b1ef1f2636671a63d13113e1
Ray Dennis <ray.dennis@intel.com>
2022-08-15 10:06:55 -0600

    added VIA support

--- keyboards/keebio/iris/keymaps/raydennis/config.h
+++ keyboards/keebio/iris/keymaps/raydennis/config.h
@@ -22,3 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+
+// https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
--- keyboards/keebio/iris/keymaps/raydennis/rules.mk
+++ keyboards/keebio/iris/keymaps/raydennis/rules.mk
@@ -1,5 +1,2 @@
-ENABLE_VIA = yes
-
-# Reduce firmware size
-# 	https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
-LTO_ENABLE = yes
+# LEADER_ENABLE = yes
+VIA_ENABLE = yes
